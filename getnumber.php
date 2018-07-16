761e736780ce2d2377b34c65301fdc7ba77c870b
root <root@terminusest.lan>
2018-07-16 06:17:10 +0200

    Changed to mysqli and added config-file for creds.

--- getnumber.php
+++ getnumber.php
@@ -1,31 +1,31 @@
 <?php
 function getExceptions() {
-	$con = mysql_connect('localhost', 'root', 'password');
-	if (!$con) {
-  		die('Could not connect: ' . mysql_error());
+	$config = parse_ini_file('config.ini');
+	$con = mysqli_connect($config['servername'], $config['username'], $config['password'], $config['dbname']);
+	if (mysqli_connect_errno()) {
+  		die('Could not connect: ' . mysqli_connect_error());
   	}
-	mysql_select_db("lodd", $con);
 	$sql="SELECT nummer FROM trukket ORDER BY nummer ASC";
 	$return = array();	
-	$result = mysql_query($sql);
-	while($row = mysql_fetch_array($result))
+	$result = mysqli_query($con, $sql);
+	while($row = mysqli_fetch_array($result))
   	{
 		array_push($return,$row[0]);
 	}
 
-	mysql_close($con);
+	mysqli_close($con);
 	return $return;	
 }
 
 function addException($exception) {
-	$con = mysql_connect('localhost', 'root', 'password');
-	if (!$con) {
-  		die('Could not connect: ' . mysql_error());
+	$config = parse_ini_file('config.ini');
+	$con = mysqli_connect($config['servername'], $config['username'], $config['password'], $config['dbname']);
+	if (mysqli_connect_errno()) {
+  		die('Could not connect: ' . mysqli_connect_error());
   	}
-	mysql_select_db("lodd", $con);
 	$sql="INSERT INTO trukket VALUES (".$exception.")";
-	mysql_query($sql);
-	mysql_close($con);
+	mysqli_query($con, $sql);
+	mysqli_close($con);
 }
 
 function randWithout($from, $to, array $exceptions) {
